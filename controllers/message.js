83935c7f02226d237517c828630c860a617ca47e
Manish Kumar Mishra <manishmishra1898@gmail.com>
2025-06-10 01:51:56 +0530

    abcd

--- controllers/message.js
+++ controllers/message.js
@@ -2,21 +2,18 @@ const User = require("../models/user.js");
 const Message = require("../models/message.js");
 
 module.exports.inbox = async (req, res) => {
-  const allMessages = await Message.find()
-    .sort({ createdAt: 1 })
-    .populate("sender");
-  res.render("./chatroom/index.ejs", { allMessages });
+  res.render("./chatroom/index.ejs");
 };
 
-module.exports.sendMessage = async (req, res) => {
-  const newMsg = new Message(req.body);
-  newMsg.sender = req.user;
-  await newMsg.save();
-  res.redirect(`/inbox`);
-};
+// module.exports.sendMessage = async (req, res) => {
+//   const newMsg = new Message(req.body);
+//   newMsg.sender = req.user;
+//   await newMsg.save();
+//   res.redirect(`/inbox`);
+// };
 
-module.exports.deleteMessage = async (req, res, next) => {
-  let { id } = req.params;
-  let deleteMsg = await Message.findByIdAndDelete(id);
-  res.redirect("/inbox");
-};
+// module.exports.deleteMessage = async (req, res, next) => {
+//   let { id } = req.params;
+//   let deleteMsg = await Message.findByIdAndDelete(id);
+//   res.redirect("/inbox");
+// };
--- routes/message.js
+++ routes/message.js
@@ -5,9 +5,10 @@ const { isLoggedIn, isOwner, validateMessage } = require("../middleware.js");
 const messageController = require("../controllers/message.js");
 
 router.route("/").get(isLoggedIn, wrapAsync(messageController.inbox));
+//   .post(isLoggedIn, validateMessage, wrapAsync(messageController.sendMessage));
 
-router
-  .route("/:id")
-  .delete(isLoggedIn, isOwner, wrapAsync(messageController.deleteMessage));
+// router
+//   .route("/:id")
+//   .delete(isLoggedIn, isOwner, wrapAsync(messageController.deleteMessage));
 
 module.exports = router;
